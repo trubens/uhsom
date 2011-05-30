07b7f1d23a9f4f98e6be07eec880f51c1bc6094e
Peder Skeidsvoll <truben@gmail.com>
2011-05-30 15:20:10 +0200

    Added HTML output. Not very functional yet.

--- src/no/truben/uhs/UHSReader.java
+++ src/no/truben/uhs/UHSReader.java
@@ -7,6 +7,7 @@ import no.truben.uhs.format.Hunk;
 import no.truben.uhs.format.Parser;
 import no.truben.uhs.format.parserImpl.EasyParser;
 import no.truben.uhs.view.HintView;
+import no.truben.uhs.view.impl.HTMLView;
 import no.truben.uhs.view.impl.HierarchicalText;
 
 
@@ -17,7 +18,8 @@ public class UHSReader {
 			//Hunk h = parser.parse(new File("tunguska.uhs"));
 			Hunk h = parser.parse(new File("brokensword-dc.uhs"));
 			
-			HintView hv = new HierarchicalText();
+			//HintView hv = new HierarchicalText();
+			HintView hv = new HTMLView();
 			hv.DisplayHints(h);
 		} catch (IOException e) {
 			e.printStackTrace();
